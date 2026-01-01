d2a2ef7921e5fe43749b4928956978657a354977
CodeTechGuy <vishalprajwal.s@gmail.com>
2026-01-01 19:00:55 +0530

    basic variables

---
+++ day02.js
@@ -0,0 +1,11 @@
+let name = "Vishal Prajwal S";
+let age = 20;
+const isStudent = true;
+let favoriteProgrammingLang = "JavaScript";
+// isStudent = false; // This will cause an error because isStudent is a constant
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("Favorite Programming Language:", favoriteProgrammingLang);
+
