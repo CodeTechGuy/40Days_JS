8b59f6cb2bf314ec7d297550c33d76a399d5b63c
CodeTechGuy <vishalprajwal.s@gmail.com>
2026-02-09 22:24:26 +0530

    basic if-else

--- day01.js
+++ day01.js
@@ -2,3 +2,12 @@ console.log("Hello, tapaScript!");
 console.log("Welcome to 40 Days of JavaScript!");
 document.write("Check the browser console for a message!");
 
+
+let color = "red"
+if(color=="red"){
+    console.log("Stop")
+} else if(color=="yellow"){
+    console.log("Slow Down")
+} else if(color=="green"){
+    console.log("Go")
+}   
