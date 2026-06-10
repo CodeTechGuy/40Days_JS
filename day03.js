660ac6fc02f1d57265a28e8afecca1e4335cac56
CodeTechGuy <vishalprajwal.s@gmail.com>
2026-06-10 23:10:49 +0530

    formatting

--- day03.js
+++ day03.js
@@ -1,12 +1,12 @@
 let num = 9
 
-if(num%2==0){
+if (num % 2 == 0) {
     console.log(`${num} is Even Number`);
-}else{
+} else {
     console.log(`${num} is Odd Number`);
 }
 
-let age=21
+let age = 21
 
 age >= 18 ? console.log("You are eligible to have driving license") : console.log("You are not eligible to have driving license");
 
@@ -18,34 +18,34 @@ age >= 18 ? console.log("You are eligible to have driving license") : console.lo
 
 let monthlySalary = 12300
 let annualSalary = monthlySalary * 12
-let bonus = 0.2*annualSalary
+let bonus = 0.2 * annualSalary
 let totalIncome = annualSalary + bonus
 console.log(`Your annual CTC is ${totalIncome}`)
 
 
 let options = ["green", "red", "yellow"]
 let color = options[Math.floor(Math.random() * options.length)]
-if(color=="red"){
-    console.log("Stop") 
-} else if(color=="yellow"){
+if (color == "red") {
+    console.log("Stop")
+} else if (color == "yellow") {
     console.log("Slow Down")
-} else if(color=="green"){
+} else if (color == "green") {
     console.log("Go")
-}  
+}
 
 
 let units = 2
 let chargePerUnit = 150
 let monthlyUnits = units * 31
 let totalMonthlyCharge = monthlyUnits * chargePerUnit
-let totalAnnualCharge = (totalMonthlyCharge * 12 )*0.8
+let totalAnnualCharge = (totalMonthlyCharge * 12) * 0.8
 console.log(`Your annual electricity bill is ${totalAnnualCharge}`)
 
 
 let year = 2200
-if(year%4==0 && year%100!=0 || year%400==0){
+if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
     console.log(`${year} is a Leap Year`);
-} else{
+} else {
     console.log(`${year} is not a Leap Year`);
 }
 
@@ -53,11 +53,11 @@ let p = 1
 let q = 2
 let r = 3
 
-if(p>=q && p>=r){
+if (p >= q && p >= r) {
     console.log(`${p} is the largest number`);
-} else if(q>=p && q>=r){
+} else if (q >= p && q >= r) {
     console.log(`${q} is the largest number`);
-} else{
+} else {
     console.log(`${r} is the largest number`);
 }
 
@@ -73,22 +73,22 @@ abstract syntax tree
 source code -> tokenize -> parsing -> AST -> Machine code
 
 && - can first option be converted to false return false	
-     can't convert first option then return second option
+    can't convert first option then return second option
      console.log("cow" && "horse") // horse
 
 || - can first option be converted to true return true
-     can't convert first option then return second option
+    can't convert first option then return second option
      console.log("cow" || "horse") // cow
 
 ?? - null/undefined 
 
 */
-function sencap(str){
+function sencap(str) {
     let senArr = str.split(" ")
     let capSen = []
-    for(let i=0; i<senArr.length; i++){
+    for (let i = 0; i < senArr.length; i++) {
         capSen.push(senArr[i].charAt(0).toUpperCase() + senArr[i].slice(1))
-    }   
+    }
     return capSen.join(" ")
 }
 let sen = "Hello world from javaScript"
--- day04.js
+++ day04.js
@@ -1,31 +1,31 @@
 let day = "Monday";
 
 switch (day) {
-   case "monday":
-       console.log("It's the start of the week.");
-       break;
-   default:
-       console.log("It's a normal day.");
+    case "monday":
+        console.log("It's the start of the week.");
+        break;
+    default:
+        console.log("It's a normal day.");
 }
 
 // let prompt = require("prompt-sync")();
-function ques02(){
+function ques02() {
     let amt = prompt("enter a amount to withdraw: ");
 
-    amt%100==0 ? console.log("Withdrawal successful") : console.log("Please enter the amount in multiples of 100");
+    amt % 100 == 0 ? console.log("Withdrawal successful") : console.log("Please enter the amount in multiples of 100");
 }
 
 
-function ques03(){
+function ques03() {
     let ops = prompt("Enter operation (+, -, *, /.%): ");
     let num1 = parseFloat(prompt("Enter first number: "));
     let num2 = parseFloat(prompt("Enter second number: "));
     let result;
 
-    switch (ops) {      
+    switch (ops) {
         case "+":
             result = num1 + num2;
-            console.log(`The sum is: ${result}`);   
+            console.log(`The sum is: ${result}`);
             break;
         case "-":
             result = num1 - num2;
@@ -54,29 +54,29 @@ function ques03(){
 
 
 let age = 25
-if(age<18){
+if (age < 18) {
     console.log("$3")
-}else if(age>=18 && age<=60){
+} else if (age >= 18 && age <= 60) {
     console.log("$10")
-} else if(age>60){
+} else if (age > 60) {
     console.log("$8")
 }
 
 
-function ques05(){
+function ques05() {
     const zodiac = {
-    january: "Capricorn",
-    february: "Pisces",
-    march: "Aries",
-    april: "Aries",
-    may: "Taurus",
-    june: "Gemini",
-    july: "Cancer",
-    august: "Leo",
-    september: "Virgo",
-    october: "Libra",
-    november: "Scorpio",
-    december: "Sagittarius"
+        january: "Capricorn",
+        february: "Pisces",
+        march: "Aries",
+        april: "Aries",
+        may: "Taurus",
+        june: "Gemini",
+        july: "Cancer",
+        august: "Leo",
+        september: "Virgo",
+        october: "Libra",
+        november: "Scorpio",
+        december: "Sagittarius"
     };
 
     const month = prompt("Enter your birth month").toLowerCase();
@@ -91,7 +91,7 @@ ques05();
 /*
 
 && - can first option be converted to false return false	
-     can't convert first option then return second option
+    can't convert first option then return second option
 
 ?? - null/undefined
 
