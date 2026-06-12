88dcb953bfc979be310d01c826f114d4f4f8962a
CodeTechGuy <vishalprajwal.s@gmail.com>
2026-06-12 19:14:54 +0530

    task problem

--- day14(try-catch).js
+++ day14(try-catch).js
@@ -53,3 +53,114 @@ function divide(a, b) {
     return a / b;
 }
 
+// ACTIVITY
+// 3. Implement a custom error handling system for an e-commerce website that categorizes errors as
+// UserError
+// PaymentError
+// ServerError
+// EmailError
+
+// Base Error
+function AppError(message) {
+    this.name = "AppError";
+    this.message = message;
+}
+
+AppError.prototype = Object.create(Error.prototype);
+AppError.prototype.constructor = AppError;
+
+
+// User Error
+function UserError(message) {
+    this.name = "UserError";
+    this.message = message;
+}
+
+UserError.prototype = Object.create(AppError.prototype);
+UserError.prototype.constructor = UserError;
+
+
+// Payment Error
+function PaymentError(message) {
+    this.name = "PaymentError";
+    this.message = message;
+}
+
+PaymentError.prototype = Object.create(AppError.prototype);
+PaymentError.prototype.constructor = PaymentError;
+
+
+// Server Error
+function ServerError(message) {
+    this.name = "ServerError";
+    this.message = message;
+}
+
+ServerError.prototype = Object.create(AppError.prototype);
+ServerError.prototype.constructor = ServerError;
+
+
+// Email Error
+function EmailError(message) {
+    this.name = "EmailError";
+    this.message = message;
+}
+
+EmailError.prototype = Object.create(AppError.prototype);
+EmailError.prototype.constructor = EmailError;
+
+// Central error handlerfunction 
+function handleError(error) {
+
+    if (error instanceof UserError) {
+        console.log("User Error:", error.message);
+    }
+
+    else if (error instanceof PaymentError) {
+        console.log("Payment Error:", error.message);
+    }
+
+    else if (error instanceof ServerError) {
+        console.log("Server Error:", error.message);
+    }
+
+    else if (error instanceof EmailError) {
+        console.log("Email Error:", error.message);
+    }
+
+    else {
+        console.log("Unknown Error:", error.message);
+    }
+}
+
+// Real E-Commerce Example
+function placeOrder(user, payment, email) {
+
+    if (!user) {
+        throw new UserError("User not logged in");
+    }
+
+    if (!payment) {
+        throw new PaymentError("Payment failed");
+    }
+
+    if (!email) {
+        throw new EmailError("Email service unavailable");
+    }
+
+    const serverDown = false;
+
+    if (serverDown) {
+        throw new ServerError("Order service unavailable");
+    }
+
+    return "Order placed successfully";
+}
+
+try {
+    placeOrder(null, true, true);
+}
+catch(error) {
+    handleError(error);
+}
+
